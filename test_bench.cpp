703ab2dc9a3136c41f8a695f8008990364bc4f5e
AaronSigal <asigal55@gmail.com>
2019-09-06 22:52:00 -0500

    Added Catch2 to project and wrote some simple tests for Bimap

--- test_bench.cpp
+++ test_bench.cpp
@@ -1,9 +1,28 @@
+#include <string>
+#include <vector>
+
 #include "container\DataSet.h"
+#include "container\Bimap.h"
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "testing\catch.hpp"
 
 using namespace std;
 
-int main() {
+TEST_CASE( "Bimap can be instanciated", "[Bimap]" ) {
+
+    Bimap<long double, string> bm;
+
+    REQUIRE( bm.size() == 0);
+
+    SECTION ("SAME KEY AND VALUE TYPE") {
+        Bimap<long double, long double> mb_ld_ld;
+        REQUIRE(bm_ld_ld.size() == 0);
+    }
+}
+
+TEST_CASE( "Bimap can assign a key-value pair" ) {
+    Bimap<long double, string>
 
 
-    return 0;
 }
